1a6d3cb15f081455f65045ba8a55a3dbb4daa3a3
Andrea Jane Mariñas <60341339+AndreaMarinas05@users.noreply.github.com>
2021-10-17 23:20:45 +0800

    PYTHON CODE

---
+++ pyth.py
@@ -0,0 +1,13 @@
+import sys
+
+while True:
+    try_again = input('\nTry again? Y/N: ')
+    try_again = try_again.lower()
+    
+    if try_again == 'y':
+    elif try_again == 'n':
+        print('Thank you for trying Triple Threat IP program.')
+        print('Ferrer, Mariñas, Trazo | 4ITD')
+        sys.exit()
+    else:
+        try_again

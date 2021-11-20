67b50f1bf898fb81e37b3527e09f42fcb91eaf79
d3gerv <60596339+d3gerv@users.noreply.github.com>
2021-11-20 18:55:50 +0800

    LoginFeature

--- sample.py
+++ sample.py
@@ -2,27 +2,44 @@ from requests import get
 import sys
 
 def main():
-    name = (input('Enter your first name: '))
+    attempts = 0
 
-    try:
-        from urllib.request import urlopen
-    except ImportError:
-        from urllib2 import urlopen
+    while True: 
+        userName = input("Username: ") 
+        password = input("Password: ")
+        attempts += 1
 
-    ip = get('https://api.ipify.org').content.decode('utf8')
-    api_key = '1187709'
-    api_url = 'https://geo.ipify.org/api/v2/country?apiKey=at_BKm9FLOgSF8SSbBYIwrPlHwlYLW8t&ipAddress=8.8.8.8'
+        #Successful Login
+        if userName == 'NetTech1' and password == 'devOps2':
+        
+            name = (input('Enter your first name: '))
 
-    url = api_url + 'apiKey=' + api_key + '&ipAddress=' + ip
+            try:
+                from urllib.request import urlopen
+            except ImportError:
+                from urllib2 import urlopen
 
-    print('\nHere are the IP details of', name, ':')
-    print_url = urlopen(url).read().decode('utf8')
+            ip = get('https://api.ipify.org').content.decode('utf8')
+            api_key = '1187709'
+            api_url = 'https://geo.ipify.org/api/v2/country?apiKey=at_BKm9FLOgSF8SSbBYIwrPlHwlYLW8t&ipAddress=8.8.8.8'
 
-    content = print_url.replace(",", "\n")
-    content2 = content.replace('"', " ")
-    content3 = content2.replace('{', " ")
-    content4 = content3.replace('}', " ")
-    print(content4)
+            url = api_url + 'apiKey=' + api_key + '&ipAddress=' + ip
+
+            print('\nHere are the IP details of', name, ':')
+            print_url = urlopen(url).read().decode('utf8')
+
+            content = print_url.replace(",", "\n")
+            content2 = content.replace('"', " ")
+            content3 = content2.replace('{', " ")
+            content4 = content3.replace('}', " ")
+            print(content4)
+
+        else:
+            if attempts == 3:
+                print('Account locked.')
+                break
+            else:
+                print('Wrong username or password. Try again. \n')
 
 main()
 
